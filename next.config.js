9435ede207df56142045eecad0a723dacfcc354c
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-04 19:38:06 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,10 @@
+const path = require("path");
 const withImages = require('next-images')
-module.exports = withImages()
+module.exports = withImages({
+    webpack(config, options) {
+        config.resolve.alias['base'] = path.join(__dirname, 'base')
+        config.resolve.alias['components'] = path.join(__dirname, 'components')
+        config.resolve.alias['assets'] = path.join(__dirname, 'assets')
+        return config;
+    }
+})
