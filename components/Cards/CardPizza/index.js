7a837028c3013fce044b0f25dad634d0ae6d3d55
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-04 19:37:21 +0300

    mod CardPizza

--- components/Cards/CardPizza/index.js
+++ components/Cards/CardPizza/index.js
@@ -1,18 +1,18 @@
 import styled from 'styled-components'
-import { h2 } from '../../base/mixins/text'
-import pricelize from '../../base/utils/pricelize'
-import img_pizza from '../../assets/images/img_pizza.jpg'
+import { h2 } from 'base/mixins/text'
+import pricelize from 'base/utils/pricelize'
+import img_pizza from 'assets/images/img_pizza.jpg'
 // import Link from 'next/link';
 
-const CardPizza = () => {
+const CardPizza = ({item: {price, title}}) => {
     return (
         <Wrapper>
             <Image src={img_pizza}/>
             <Content>
-                <Title>Pepperoni</Title>
+                <Title>{title}</Title>
                 <Text>Spicy pepperoni, large portion of mozzarella, tomato sauce</Text>
                 <ContentBottom>
-                    <Title>{pricelize(5)} $</Title>
+                    <Title>{pricelize(price)} $</Title>
                     {/* <Button></Button> */}
                 </ContentBottom>
             </Content>
@@ -25,10 +25,10 @@ const Wrapper = styled.div`
     cursor: pointer;
     border-radius: 20px;
     padding: 10px;
-    transition: box-shadow 0.2s;
+    /* transition: box-shadow 0.2s;
     &:hover {
         box-shadow: 0 0 10px 5px  ${p => p.theme.color.secondary};
-    }
+    } */
 `
 
 const Title = styled.div`
@@ -51,4 +51,5 @@ const Image = styled.img`
 const Content = styled.div``
 
 const ContentBottom = styled.div``
+
 export default CardPizza
