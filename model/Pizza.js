85fd5481b2232ca14d0562a3ea24cfe5eadd968f
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-04 19:38:17 +0300

    Create Pizza.js

---
+++ model/Pizza.js
@@ -0,0 +1,25 @@
+const list = async () => {
+   return [{
+        title: "Margarita",
+        price: 5
+    }, {
+        title: "Pepperoni",
+        price: 6
+    }, {
+        title: "Pepperoni 1",
+        price: 5
+    }, {
+        title: "Pepperoni 2",
+        price: 10
+    }, {
+        title: "Pepperoni 3",
+        price: 2
+    }, {
+        title: "Pepperoni 4",
+        price: 3
+    }]
+}
+
+module.exports = {
+    list
+}
