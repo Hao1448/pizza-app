29d628301287a37009df4d755b987418de8cf8bd
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-06 18:31:10 +0300

    Update Header

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,27 +1,52 @@
 import styled from 'styled-components'
 import { h1 } from 'base/mixins/text'
-import { Container } from 'components'
+import { Container, UiButton, Grid } from 'components'
 
 const Header = () => {
     return (
         <Wrapper>
-            <Container> 
-                <Title>
-                    Choose your Pizza!
-                </Title>
-            </Container>  
+            <Container>
+                <Grid>
+                    <Row className='title'>
+                        <Title>Pizzeria Pizza App</Title>
+                    </Row>
+                    <Row className='button'>
+                        <UiButton wide="true" type={'technical'}>Login</UiButton>
+                    </Row>
+                    <Row className='button '>
+                        <UiButton wide="true">Cart</UiButton>
+                    </Row>
+                </Grid>
+            </Container>
         </Wrapper>
     )
 }
 
 
-const Wrapper = styled.div`
-    
+const Wrapper = styled.div``
+
+const Row = styled.div`
+   &.title {
+        grid-column: span 6;
+        grid-row: 1 / 3;
+        align-self: center;
+   }
+   &.button {
+        grid-column: 9 / span 3;
+        & + & {
+            margin-top: 10px;
+        }
+    }
 `
+
+
 const Title = styled.div`
     ${h1};
+    text-transform: uppercase;
     text-align: center;
     color: ${p => p.theme.color.primary};
+    font-weight: bold;
 `
+
 
 export default Header
