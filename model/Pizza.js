76a900d75a3aecaaf09ae9359df6f01fd891f88f
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-08 14:12:02 +0300

    Update Pizza.js

--- model/Pizza.js
+++ model/Pizza.js
@@ -1,25 +1,31 @@
+require('dotenv').config()
+
+const { Sequelize } = require('sequelize');
+
+const dbconfig = {};
+dbconfig.database = process.env.DB_NAME;
+dbconfig.username = process.env.DB_USER;
+dbconfig.password = process.env.DB_PASSWORD;
+dbconfig.hostname = process.env.DB_HOST;
+
+const sequelize = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
+  host: dbconfig.hostname,
+  dialect: 'mysql'
+});
+
+const Pizza = sequelize.define('pizzas', {
+  title: Sequelize.STRING, 
+  price: Sequelize.FLOAT,
+  text: Sequelize.STRING,
+  img: Sequelize.STRING
+});
+
+Pizza.sync();
+
 const list = async () => {
-   return [{
-        title: "Margarita",
-        price: 5
-    }, {
-        title: "Pepperoni",
-        price: 6
-    }, {
-        title: "Pepperoni 1",
-        price: 5
-    }, {
-        title: "Pepperoni 2",
-        price: 10
-    }, {
-        title: "Pepperoni 3",
-        price: 2
-    }, {
-        title: "Pepperoni 4",
-        price: 3
-    }]
+  return await Pizza.findAll();
 }
 
 module.exports = {
-    list
+  list
 }
