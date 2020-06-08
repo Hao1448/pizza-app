f399d16b03482d1fad497e333e7662091013ba7b
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-08 14:11:21 +0300

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
+import {useState, useEffect} from 'react';
 import { WidgetCards } from 'components'
+import fetch from 'isomorphic-unfetch';
 
 export default function Index() {
-  return <WidgetCards />
+  const [arrPizza, setArrPizza] = useState();
+
+    useEffect(() => {
+        fetch('/api/pizza')
+            .then(r => r.json())
+            .then(data => setArrPizza(data))
+    }, [])
+
+    if(!arrPizza) {
+        return 'Loading...'
+    }
+  return <WidgetCards pizzas={arrPizza}/>
 }
