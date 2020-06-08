d97aa68eb8061a4c9698d347d36817396131fd06
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-08 14:12:05 +0300

    Update index.js

--- components/index.js
+++ components/index.js
@@ -1,9 +1,12 @@
 import Header from './Header'
 import Container from './Container'
 import Grid from './Grid'
+import UiIcon from './Icon'
 
 export * from './Cards'
 export * from './Widgets'
 export * from './Buttons'
+export * from './Fields'
+export * from './Forms'
 
-export { Grid, Container, Header }
+export { Grid, Container, Header, UiIcon }
