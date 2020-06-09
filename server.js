84151e19e1fb2d43a22246fab1cba48c34600b4f
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-09 16:11:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 require('dotenv').config()
+const bodyParser = require('body-parser')
 const express = require('express')
 const next = require('next')
 
@@ -8,13 +9,43 @@ const app = next({ dev })
 const handle = app.getRequestHandler()
 
 const Pizza = require('./model/Pizza');
+const Order = require('./model/Order');
 
 app.prepare().then(() => {
   const server = express()
 
+  server.use(bodyParser.json())
+
   server.get('/api/pizza', async (req, res) => {
     const pizza = await Pizza.list();
     res.json(pizza);
+  })
+
+  server.get('/api/orders', async(req, res) => {
+    const orders = await Order.list();
+    res.json(orders);
+  })
+
+  server.get('/api/order/:id', async(req,res) => {
+    const order = await Order.getById(req.params.id);
+    res.json(JSON.parse(order.data));
+  })
+
+  server.post('/api/order', async (req, res) => {
+    const { address, pizzas } = req.body;
+    const order = await Order.createOrder({
+      data: JSON.stringify({
+        address,
+        pizzas
+      })
+    });
+
+    res.json({
+      data: {
+        id: order.id
+      },
+      success: true
+    });
   })
 
   server.all('*', (req, res) => {
