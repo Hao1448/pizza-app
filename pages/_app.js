ad23857074f18a66ddad7693810d322d98fd70e9
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-09 16:12:10 +0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,31 +2,18 @@ import React, { Fragment } from 'react'
 import styled, { ThemeProvider } from 'styled-components'
 import { CardProvider } from 'base/cardContext'
 import App from 'next/app'
-// import Router from 'next/router'
-import Head from 'next/head'
-// import dynamic from 'next/dynamic'
 import { GlobalTheme } from 'base/theme'
 import { GlobalStyle } from 'base/styles'
 import { Header } from 'components'
-
-// import('../static/pwa/pwa.js')
 
 
 class MyApp extends App {
     render() {
         const { Component, pageProps } = this.props
         return (
-            // <Provider>
                 <ThemeProvider theme={GlobalTheme}>
                     <Fragment>
                         <GlobalStyle />
-                        <Head>
-                            {/* <meta
-                                id="viewport"
-                                name="viewport"
-                                content="initial-scale=1.0, width=device-width"
-                            /> */}
-                        </Head>
                         <Wrapper>
                             <CardProvider>
                                 <Header />
@@ -37,7 +24,6 @@ class MyApp extends App {
                         </Wrapper>
                     </Fragment>
                 </ThemeProvider>
-            // </Provider>
         )
     }
 }
