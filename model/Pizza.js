137df1307d7108e48365e38267f65c21e5a70ae6
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-09 16:12:02 +0300

    Update Pizza.js

--- model/Pizza.js
+++ model/Pizza.js
@@ -1,17 +1,5 @@
-require('dotenv').config()
-
 const { Sequelize } = require('sequelize');
-
-const dbconfig = {};
-dbconfig.database = process.env.DB_NAME;
-dbconfig.username = process.env.DB_USER;
-dbconfig.password = process.env.DB_PASSWORD;
-dbconfig.hostname = process.env.DB_HOST;
-
-const sequelize = new Sequelize(dbconfig.database, dbconfig.username, dbconfig.password, {
-  host: dbconfig.hostname,
-  dialect: 'mysql'
-});
+const { sequelize } = require('./db');
 
 const Pizza = sequelize.define('pizzas', {
   title: Sequelize.STRING, 
