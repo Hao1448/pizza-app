9c39c9e2b6a7630fbed6c2b2236b2dcaf4120fd1
Ilya Kvitkov <ziker2009gtx@gmail.com>
2020-06-09 16:10:49 +0300

    Update index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -1,5 +1,6 @@
 import CardPizza from './CardPizza'
 import CardEmptyCart from './CardEmptyCart'
 import CardCart from './СardCart'
+import CardOrder from './CartOrder'
 
-export { CardPizza, CardEmptyCart, CardCart }
+export { CardPizza, CardEmptyCart, CardCart, CardOrder }
